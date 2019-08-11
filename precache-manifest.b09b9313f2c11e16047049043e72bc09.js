a82de5f04e82d5a4594b6c11e256a46b97ec06f2
Michael Brünen <mb156208@fh-muenster.de>
2019-08-11 15:32:18 +0200

    Updates

---
+++ precache-manifest.b09b9313f2c11e16047049043e72bc09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c8a1b637c9d6710bef9d918a2a9937",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "cd98951d85d79ee7b4fa",
+    "url": "/Markdown-Previewer/static/css/main.3d3a64a1.chunk.css"
+  },
+  {
+    "revision": "50b29d639ba33a9ba010",
+    "url": "/Markdown-Previewer/static/js/2.868bd5d5.chunk.js"
+  },
+  {
+    "revision": "cd98951d85d79ee7b4fa",
+    "url": "/Markdown-Previewer/static/js/main.2874a94e.chunk.js"
+  },
+  {
+    "revision": "416a15c39f7dc43ffd3b",
+    "url": "/Markdown-Previewer/static/js/runtime~main.b6ea0a3f.js"
+  }
+]);
